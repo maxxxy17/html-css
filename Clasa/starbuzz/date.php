d6c3934609ce626261388e07e56eeee8a9bfc20a
maxxxy17 <mihalceaionut17@yahoo.com>
2018-12-13 20:54:17 +0200

    13.121.2018

---
+++ Clasa/starbuzz/date.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<?php
+date_default_timezone_set("Europe/Bucharest");
+	
+	echo date("Y-m-d H:i:s") . "<br />";
+	
+	$colors = ["red", "green", "blue", "yellow", "magenta", "black"];
+	foreach($colors as $color) {
+		echo "<div style=\"color: $color;\">$color</div>";
+	}
+
+for($j=1; $j<=9; $j++) {	
+	echo "<div>";
+	for($i=1; $i<=9; $i++)
+	{
+		echo "$j x $i = " . $j * $i . '<br />';
+	}
+	echo "</div>";
+}
+	
+?>
+<body>
+</body>
+</html>
